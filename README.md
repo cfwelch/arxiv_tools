35ded9a5193e193dcb399868ccf8aaeecd96fc23
Charlie Welch <cwelch@mathematik.uni-marburg.de>
2022-12-13 12:53:38 +0100

    Automatic repository sync @ pc12740

---
+++ README.md
@@ -0,0 +1,5 @@
+If you have a bunch of unread arXiv updates, you can download them as EML files and put them in the emls folder. Then you can run the read_arxiv.py to extract newly listed articles and cross-listings as a CSV that is importable to Notion.
+
+In gmail you can download emls individually from within the email, or you can select the emails you want to download, forward them to yourself as an attachment, then download them in bulk as a zip and extract them to the emls folder.
+
+The script skips papers that were replaced and ignores email headers.
--- read_arxiv.py
+++ read_arxiv.py
@@ -1,32 +1,59 @@
 
+import os
 from dateutil.parser import parse
 
-FILENAME = 'test'
+EMAIL_FOLDER = 'emls'
+DATE_DELIMITER = '   '
 
 def main():
+    file_list = [t for t in os.listdir(EMAIL_FOLDER) if t.endswith('.eml')]
+    entry_set = []
+    print(file_list)
+
+    for file in file_list:
+        entry_set.extend(parse_email(file))
+
+    for entry in entry_set:
+        entry['title'] = entry['title'].replace('"', '\'')
+        entry['authors'] = entry['authors'].replace('"', '\'')
+        entry['abstract'] = entry['abstract'].replace('"', '\'')
+
+    with open('output_arxiv.csv', 'w') as handle:
+        handle.write('Title,Authors,Month,Year,URL,Abstract,First Pass\n')
+        for entry in entry_set:
+            handle.write('"' + entry['title'] + '","' + entry['authors'] + '",' + str(entry['month']) + ',' + str(entry['year']) + ',"' + entry['url'] + '","' + entry['abstract'] + '","No"\n')
+
+def parse_email(filename):
     lines = []
-    with open(FILENAME) as handle:
+    with open(EMAIL_FOLDER + '/' + filename) as handle:
         lines = handle.readlines()
 
     entry_set = []
     abstract = False
     title = False
     authors = False
+    skip_entry = False
     entry = {}
     for line in lines:
         if line.startswith('-----------'):
-            if len(entry) > 0:
+            if len(entry) > 0 and not skip_entry:
                 entry_set.append(entry)
                 print(entry)
                 entry = {}
                 abstract = False
 
+        if line.startswith('replaced with revised'):
+            skip_entry = True
+
         if line.startswith('Date: '):
-            ds = line[6:line.index('   ')]
-            dt = parse(ds)
-            # print(dt)
-            entry['month'] = dt.month
-            entry['year'] = dt.year
+            if DATE_DELIMITER in line:
+                ds = line[6:line.index(DATE_DELIMITER)]
+                dt = parse(ds)
+                # print(dt)
+                entry['month'] = dt.month
+                entry['year'] = dt.year
+            else:
+                print('Date is part of external data -- Skipping...')
 
         if line.startswith('Title: '):
             entry['title'] = line.strip()[7:]
@@ -49,7 +76,7 @@ def main():
                 entry['title'] += ' ' + line.strip()
             elif authors:
                 entry['authors'] += ' ' + line.strip()
-            else:
+            elif 'title' in entry: ### skips lines with spaces that are outside of an entry
                 abstract = True
 
         if abstract:
@@ -62,15 +89,7 @@ def main():
             if line.startswith('\\\\'):
                 del entry['abstract']
 
-    for entry in entry_set:
-        entry['title'] = entry['title'].replace('"', '\'')
-        entry['authors'] = entry['authors'].replace('"', '\'')
-        entry['abstract'] = entry['abstract'].replace('"', '\'')
-
-    with open('output_arxiv.csv', 'w') as handle:
-        handle.write('Title,Authors,Month,Year,URL,Abstract,First Pass\n')
-        for entry in entry_set:
-            handle.write('"' + entry['title'] + '","' + entry['authors'] + '",' + str(entry['month']) + ',' + str(entry['year']) + ',"' + entry['url'] + '","' + entry['abstract'] + '","No"\n')
+    return entry_set
 
 if __name__ == '__main__':
     main()
